faba9d8f28f3562901509b4174b98b6b4b9b6aa5
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-02-04 12:18:11 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -1,9 +1,10 @@
 # config.py
 
 api_key = 'hlRsc8uhOpdJ2UoKZ6FXCMjIzEMjINRw3TdPLKrikXwo9yBf4pY53kyM4SYtJPwr'
-api_secret = 'your_api_secret'
+api_secret = 'vhKdU8BkHvN8oU0D881yFjNOvvHT9J45RQPWV8zw5ixFLJNwiHG86WdoYPmZxXT0'
 
 # Binance Futures Configurations
-futures_symbol = 'BTC/USDT'
+
 futures_leverage = 10
-futures_fixed_quantity_usdt = 100
+futures_fixed_quantity_usdt = 11
+futures_symbol = 'BTC/USDT'
