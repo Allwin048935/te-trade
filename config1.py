dbec1fc2510a141126286fd222c8536b7ba09481
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-02-04 12:16:55 +0530

    Create config1.py

---
+++ config1.py
@@ -0,0 +1,9 @@
+# config.py
+
+api_key = 'your_api_key'
+api_secret = 'your_api_secret'
+
+# Binance Futures Configurations
+futures_symbol = 'BTC/USDT'
+futures_leverage = 10
+futures_fixed_quantity_usdt = 100
