a2c66db621241e7d253e207636e03ae8730d9f47
Allwin048935 <156902166+Allwin048935@users.noreply.github.com>
2024-02-04 12:17:35 +0530

    Update config1.py

--- config1.py
+++ config1.py
@@ -1,6 +1,6 @@
 # config.py
 
-api_key = 'your_api_key'
+api_key = 'hlRsc8uhOpdJ2UoKZ6FXCMjIzEMjINRw3TdPLKrikXwo9yBf4pY53kyM4SYtJPwr'
 api_secret = 'your_api_secret'
 
 # Binance Futures Configurations
